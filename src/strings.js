46baaf2ef24495d4ac4c6537014528f4388de274
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-21 15:08:52 +0200

    Refactor: extract repeated Strings to a separate file
    Fixes #19

---
+++ src/strings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  INFO_NO_PACKAGE: 'This project has no package.json file.',
+  ERROR_VERSION_RANGE_INVALID: 'Value not within the allowed range.',
+  ERROR_PACKAGE_DIRTY:
+    'The file package.json is dirty, either save it, undo the changes or consider using the setting Auto save of the extension.',
+  ERROR_NO_VERSION_PROP: 'The package.json file does not contain the version property.',
+  ERROR_PACKAGE_INVALID:
+    "An error has occurred while updating the project's version. Check if your project's package.json file is valid.",
+  ERROR_OPEN_FILE: "An error has occurred while opening the project's package.json file.",
+}
