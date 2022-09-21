eafb4b77b8fb6e9307f73ddea192a694929cd40d
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-21 15:13:09 +0200

    Refactor: remove redundant async and clean up
    Fixes #20

--- src/extension.js
+++ src/extension.js
@@ -12,7 +12,7 @@ async function activate(context) {
 
   if (extension == null) {
     extension = new PackageInfo(context)
-    await extension.init()
+    extension.init()
   }
 
   context.subscriptions.push(
@@ -22,10 +22,10 @@ async function activate(context) {
       }),
 
       vscode.commands.registerCommand('projectVersion.refresh', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           await extension.updateStatusBarItem(true)
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
 
@@ -34,62 +34,62 @@ async function activate(context) {
       }),
 
       vscode.commands.registerCommand('projectVersion.increaseMajor', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           await extension.increaseVersion('major')
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
 
       vscode.commands.registerCommand('projectVersion.increaseMajorBy', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           const input = await extension.showIncreaseByInput('major')
 
           if (input && input !== '') {
             await extension.increaseVersion('major', +input)
           }
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
 
       vscode.commands.registerCommand('projectVersion.increaseMinor', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           await extension.increaseVersion('minor')
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
 
       vscode.commands.registerCommand('projectVersion.increaseMinorBy', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           const input = await extension.showIncreaseByInput('minor')
 
           if (input && input !== '') {
             await extension.increaseVersion('minor', +input)
           }
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
 
       vscode.commands.registerCommand('projectVersion.increasePatch', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           await extension.increaseVersion('patch')
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
 
       vscode.commands.registerCommand('projectVersion.increasePatchBy', async () => {
-        if (await extension.packageFileExists()) {
+        if (extension.hasPackage) {
           const input = await extension.showIncreaseByInput('patch')
 
           if (input && input !== '') {
             await extension.increaseVersion('patch', +input)
           }
         } else {
-          vscode.window.showInformationMessage('This project has no package.json file.')
+          extension.noPackageMessage()
         }
       }),
     ]
